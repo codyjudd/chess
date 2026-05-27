e98a2b956920dcd4f6f39e3ab72b3867894a798f
Cody Judd <your-email@example.com>
2026-05-27 01:59:03 -0600

    testing servver wiht tester class before I move  along more

---
+++ server/src/main/java/dataaccess/tester.java
@@ -0,0 +1,10 @@
+package dataaccess;
+
+public class tester {
+    public static void main(String[] args) throws Exception {
+
+        MySqlDataAccess dao = new MySqlDataAccess();
+
+        System.out.println("worked");
+    }
+}
