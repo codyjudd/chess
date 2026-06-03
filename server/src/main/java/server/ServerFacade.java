f9fba40f39597d78e61d9acb9fc8a5e0e472344c
Cody Judd <your-email@example.com>
2026-06-02 18:18:04 -0600

    register() and login()

--- server/src/main/java/server/ServerFacade.java
+++ server/src/main/java/server/ServerFacade.java
@@ -4,8 +4,8 @@ import com.google.gson.Gson;
 import exception.ResponseException;
 import model.AuthData;
 
-import java.io.OutputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
 
@@ -53,7 +53,6 @@ public class ServerFacade {
             http.addRequestProperty("Content-Type", "application/json");
 
             var request = new RegisterRequest(username, password, email);
-
             String json = gson.toJson(request);
 
             try (OutputStream output = http.getOutputStream()) {
@@ -77,9 +76,75 @@ public class ServerFacade {
         }
     }
 
+    public AuthData login(String username,
+                          String password)
+            throws ResponseException {
+
+        try {
+            var url = URI.create(serverUrl + "/session").toURL();
+
+            HttpURLConnection http =
+                    (HttpURLConnection) url.openConnection();
+
+            http.setRequestMethod("POST");
+            http.setDoOutput(true);
+            http.addRequestProperty("Content-Type", "application/json");
+
+            var request = new LoginRequest(username, password);
+            String json = gson.toJson(request);
+
+            try (OutputStream output = http.getOutputStream()) {
+                output.write(json.getBytes());
+            }
+
+            http.connect();
+
+            if (http.getResponseCode() != 200) {
+                throw new ResponseException("Login failed");
+            }
+
+            try (InputStreamReader reader =
+                         new InputStreamReader(http.getInputStream())) {
+
+                return gson.fromJson(reader, AuthData.class);
+            }
+
+        } catch (Exception e) {
+            throw new ResponseException("Connection error");
+        }
+    }
+
+    public void logout(String authToken)
+            throws ResponseException {
+
+        try {
+            var url = URI.create(serverUrl + "/session").toURL();
+
+            HttpURLConnection http =
+                    (HttpURLConnection) url.openConnection();
+
+            http.setRequestMethod("DELETE");
+            http.addRequestProperty("authorization", authToken);
+
+            http.connect();
+
+            if (http.getResponseCode() != 200) {
+                throw new ResponseException("Logout failed");
+            }
+
+        } catch (Exception e) {
+            throw new ResponseException("Connection error");
+        }
+    }
+
     private record RegisterRequest(
             String username,
             String password,
             String email) {
+    }
+
+    private record LoginRequest(
+            String username,
+            String password) {
     }
 }
