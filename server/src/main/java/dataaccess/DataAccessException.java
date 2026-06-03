6bb631cbb91bdd950a7cd6bf0a06771ff3744a58
Cody Judd <your-email@example.com>
2026-06-03 02:14:02 -0600

    Restore DataAccessException constructor

--- server/src/main/java/dataaccess/DataAccessException.java
+++ server/src/main/java/dataaccess/DataAccessException.java
@@ -4,4 +4,8 @@ public class DataAccessException extends Exception {
     public DataAccessException(String message) {
         super(message.startsWith("Error") ? message : "Error: " + message);
     }
+
+    public DataAccessException(String message, Throwable cause) {
+        super(message.startsWith("Error") ? message : "Error: " + message, cause);
+    }
 }
